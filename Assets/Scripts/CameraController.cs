da1476f811efaf5770982fc2997bb5d8c909fc01
aadame3311 <fps3311@gmail.com>
2015-12-31 00:08:42 -0600

    Unparented camera and added follow script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    GameObject player;
+
+    public float offsetY;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + offsetY, player.transform.position.z);
+        transform.rotation = player.transform.rotation;
+
+	}
+}
