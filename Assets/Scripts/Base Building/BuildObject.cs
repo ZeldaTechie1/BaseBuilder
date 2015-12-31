87ceb429e471d5e054ba37dc96bdb040b2172438
Jesus Reyes <jesrey4@outlook.com>
2015-12-31 02:12:54 -0600

    Spawning Snap Points Complete

--- Assets/Scripts/Base Building/BuildObject.cs
+++ Assets/Scripts/Base Building/BuildObject.cs
@@ -3,23 +3,57 @@ using System.Collections;
 
 public class BuildObject : MonoBehaviour {
 
-    SnapPoint[,,] snapPoints;
+
     [SerializeField]
     GameObject snapPoint_go;
+    public int dimX;
+    public int dimY;
+    public int dimZ;
 
     // Use this for initialization
     void Start () {
-        string name = this.gameObject.name;
-        int dim1 = int.Parse(name[0]+"");
-        int dim2 = int.Parse(name[2]+"");
-        int dim3 = int.Parse(name[4] + "");
-        Debug.Log("Dimensions: " + dim1 + "x" + dim2 + "x" + dim3);
-        snapPoints = new SnapPoint[dim1,dim2,dim3];
-
-	}
+        for (int count = 0; count < dimX; count++)//spawns snap points along the x axis
+        {
+            Vector3 curPosition = transform.position;
+            //spawns top two
+            SpawnSnapPoint(count, curPosition.x - ((.5f * (dimX - 1)) - (count)), curPosition.y + (.5f * dimY), curPosition.z - (.5f * dimZ));
+            SpawnSnapPoint(count, curPosition.x - ((.5f * (dimX - 1)) - count), curPosition.y + (.5f * dimY), curPosition.z + (.5f * dimZ));
+            //spawns bottom two
+            SpawnSnapPoint(count, curPosition.x - ((.5f * (dimX - 1)) - (count)), curPosition.y - (.5f * dimY), curPosition.z - (.5f * dimZ));
+            SpawnSnapPoint(count, curPosition.x - ((.5f * (dimX - 1)) - count), curPosition.y - (.5f * dimY), curPosition.z + (.5f * dimZ));
+        }
+        for (int count = 0; count < dimY; count++)//spawns snap points along the y axis
+        {
+            Vector3 curPosition = transform.position;
+            //spawns right two
+            SpawnSnapPoint(count, curPosition.x + (.5f * dimX), curPosition.y - ((.5f * (dimY - 1)) - (count)), curPosition.z - (.5f * dimZ));
+            SpawnSnapPoint(count, curPosition.x + (.5f * dimX), curPosition.y - ((.5f * (dimY - 1)) - (count)), curPosition.z + (.5f * dimZ));
+            //spawns left two
+            SpawnSnapPoint(count, curPosition.x - (.5f * dimX), curPosition.y - ((.5f * (dimY - 1)) - (count)), curPosition.z - (.5f * dimZ));
+            SpawnSnapPoint(count, curPosition.x - (.5f * dimX), curPosition.y - ((.5f * (dimY - 1)) - (count)), curPosition.z + (.5f * dimZ));
+        }
+        for (int count = 0; count < dimZ; count++)//spawns snap points along the z axis
+        {
+            Vector3 curPosition = transform.position;
+            //spawns front two
+            SpawnSnapPoint(count, curPosition.x - (.5f * dimX), curPosition.y + (.5f * dimY), curPosition.z - ((.5f * (dimZ - 1)) - (count)));
+            SpawnSnapPoint(count, curPosition.x + (.5f * dimX), curPosition.y + (.5f * dimY), curPosition.z - ((.5f * (dimZ - 1)) - (count)));
+            //spawns back two
+            SpawnSnapPoint(count, curPosition.x - (.5f * dimX), curPosition.y - (.5f * dimY), curPosition.z - ((.5f * (dimZ - 1)) - (count)));
+            SpawnSnapPoint(count, curPosition.x + (.5f * dimX), curPosition.y - (.5f * dimY), curPosition.z - ((.5f * (dimZ - 1)) - (count)));
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void SpawnSnapPoint(int count, float x, float y, float z)
+    {
+        Vector3 curPosition = transform.position;
+        Vector3 newPosition = new Vector3(x,y,z);
+        GameObject snap = Instantiate(snapPoint_go, newPosition, Quaternion.identity) as GameObject;
+        snap.transform.SetParent(this.gameObject.transform);
+    }
 }
