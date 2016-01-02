6b0f4ac8c12d84c9d7d8ac5b6ac8c1e27a2524bd
aadame3311 <fps3311@gmail.com>
2016-01-02 17:32:17 -0600

    something

---
+++ Assets/Scripts/Invisible mesh.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Invisiblemesh : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+        GetComponent<Renderer>().enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
