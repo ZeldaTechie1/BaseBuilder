74dba71967bbcc5dddfe57b54939aedc1161fbaa
aadame3311 <fps3311@gmail.com>
2016-01-03 19:24:23 -0600

    lskjdf

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,7 @@ public class PlayerController : MonoBehaviour {
     float xLook, yLook, xLookTarget, yLookTarget;
     public float moveSpeed, walkSpeed, runSpeed;
     public float mouseSensX, mouseSensY;
-    float yMax = 290, yMin = 50;
     Vector3 targetSpeed, smoothMove;
-
-    //public float smoothStop, smoothVel;
-    //float xSmooth, ySmooth;
 
     //Jump
     bool grounded;
@@ -42,6 +38,12 @@ public class PlayerController : MonoBehaviour {
 
         moveSpeed = walkSpeed;
 
+        xLook += Input.GetAxis("Mouse Y") * mouseSensY * Time.deltaTime;
+        yLook = Input.GetAxis("Mouse X") * mouseSensX * Time.deltaTime;
+
+        xMove = Input.GetAxisRaw("Horizontal");
+        zMove = Input.GetAxisRaw("Vertical");
+
         Movement();
         Jump();
 
@@ -63,30 +65,24 @@ public class PlayerController : MonoBehaviour {
 
         }
 
-        
-
-        xMove = Input.GetAxisRaw("Horizontal");
-        zMove = Input.GetAxisRaw("Vertical");
-
         Vector3 speed = new Vector3(xMove, 0, zMove);
         speed.Normalize();
 
         speed = transform.rotation * speed;
         targetSpeed = speed * moveSpeed;
-        targetSpeed = Vector3.SmoothDamp(speed, targetSpeed, ref smoothMove, .5f);
-        rb.MovePosition(rb.position + targetSpeed * Time.fixedDeltaTime);
+        speed = Vector3.SmoothDamp(speed, targetSpeed, ref smoothMove, .5f);
+
+        rb.MovePosition(rb.position + speed * Time.fixedDeltaTime);
 
     }
 
     void LookAround()
     {
 
-        xLook = Input.GetAxis("Mouse Y") * mouseSensY * Time.fixedDeltaTime;
-        yLook = Input.GetAxis("Mouse X") * mouseSensX * Time.fixedDeltaTime;
-        //xLook = Mathf.Clamp(yLook, yMin, yMax); //Stops player from infinately looking up or down
-        Debug.Log(yLook);
+        //xLook = Mathf.Clamp(xLook, 290, 315);
+
         transform.Rotate(0, yLook, 0); //Rotates player side to side
-        Camera.main.transform.Rotate(-xLook, 0, 0);//localRotation = Quaternion.Euler(-xLook, 0, 0);
+        Camera.main.transform.rotation = Quaternion.Euler(xLook, 0, 0);//Rotates camera up and down
 
     }
 
